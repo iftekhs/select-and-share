ceca1f06ee1e6934c08c325ae423ea132770498e
iftekhs <iftekhstudio@gmail.com>
2023-03-30 01:34:34 +0600

    added GoogleGeocodingResponse type getting coordinates from response and saving it to coordinates variable checking if the status is ok or not alerting error message on error

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,38 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = 'YOUR_API_KEY';
 
+type GoogleGeocodingResponse = {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  };
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
 const searchAddressHandler = (event: Event) => {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   // send this to Google's API!
   axios
-    .get(
+    .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     )
-    .then((data) => {
-      console.log(data);
+    .then((response) => {
+      if (response.data.status != 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results.geometry.location.lat;
     })
-    .catch((error) => {
-      console.log(error);
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
     });
 };
 
